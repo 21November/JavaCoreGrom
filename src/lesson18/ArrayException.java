e0b6ce1704cd46bbe8b51c00bdb18e4734457dd9
Misha Lebedev <555Leprikon555@gmail.com>
2019-05-14 21:31:06 +0300

    Added lesson18 Error handling in Java, try-catch-finally.

---
+++ src/lesson18/ArrayException.java
@@ -0,0 +1,20 @@
+package lesson18;
+
+public class ArrayException {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 0};
+
+        try {
+            System.out.println(array[2]);
+        }catch (Exception e){
+            System.err.println("element was not found");
+        }
+
+
+        System.out.println("Cool....");
+
+        System.out.println("Cool2....");
+
+
+    }
+}
