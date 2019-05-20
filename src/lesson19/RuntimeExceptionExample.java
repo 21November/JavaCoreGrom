a40f53991eb473680fe4e6f85da8df2a4174e358
Misha Lebedev <555Leprikon555@gmail.com>
2019-05-20 19:49:27 +0300

    Lesson19 practice.

---
+++ src/lesson19/RuntimeExceptionExample.java
@@ -0,0 +1,20 @@
+package lesson19;
+
+public class RuntimeExceptionExample {
+    public static void main(String[] args) {
+
+        //arithmetic exception
+        arithmeticException(2);
+        try {
+            arithmeticException(0);
+        }catch (ArithmeticException e){
+            System.out.println("something wrong");
+        }
+
+
+    }
+
+    private static void arithmeticException(int a){
+        System.out.println(10 / a);
+    }
+}
