d5c799ec7afc70982a58c9515f70a7b0c183488c
Misha Lebedev <555Leprikon555@gmail.com>
2019-05-28 20:30:11 +0300

    Added lesson22 homework

---
+++ src/lesson22/HomeWork/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package lesson22.HomeWork.exception;
+
+public class BadRequestException extends Exception{
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/lesson22/HomeWork/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package lesson22.HomeWork.exception;
+
+public class InternalServerException extends Exception{
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
